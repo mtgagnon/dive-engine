47db1a4e8dca87ba3667c5bfb6b34fecda28db2c
Mathurin Gagnon <mtgagnon@umich.edu>
2024-11-27 23:09:57 -0800

    accidentally added these

--- builds/build-debug/external/glm/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/mgagnon/dive-engine/external/glm
-# Build directory: /home/mgagnon/dive-engine/builds/build-debug/external/glm
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("glm")
--- builds/build-release/external/glm/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/mgagnon/dive-engine/external/glm
-# Build directory: /home/mgagnon/dive-engine/builds/build-release/external/glm
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("glm")
