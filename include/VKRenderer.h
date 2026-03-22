ba75abc65b0e92db98b52529bdbedf0a78637978
Mathurin Gagnon <91029118+mtgagnon@users.noreply.github.com>
2026-03-22 16:24:18 -0700

    updated gitignor for cursro files and updated vkrender.h

--- include/VKRenderer.h
+++ include/VKRenderer.h
@@ -1,5 +1,5 @@
 //
-//  VulkanRenderer.h
+//  VKRenderer.h
 //  dive_engine
 //
 
@@ -13,10 +13,23 @@
 #include <vulkan/vulkan.h>
 #include "SDL2/SDL.h"
 
+
 class VKRenderer {
 public:
+    void initialize(SDL_Window* window);
+    void cleanup();
+
+    void beginFrame();
+    void endFrame();
+
+    bool isInitialized() const { return initialized; };
 
 private:
+
+    SDL_Window* window = nullptr;
+    uint32_t currentFrame = 0;
+    uint32_t current_frame_start_timestamp = 0;
+    bool initialized = false;
 };
 
 #endif /* VKRENDERER_H */
