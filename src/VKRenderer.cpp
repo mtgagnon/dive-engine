78538662bc79eede620ef3ba693637a70a0ad84f
Mathurin Gagnon <91029118+mtgagnon@users.noreply.github.com>
2026-04-18 21:05:42 -0700

    changed vk api ver 1.2.0->1.3.0

--- src/VKRenderer.cpp
+++ src/VKRenderer.cpp
@@ -86,7 +86,7 @@ void VKRenderer::createInstance() {
     appInfo.applicationVersion = VK_MAKE_VERSION(1, 0, 0);
     appInfo.pEngineName = "Dive Engine";
     appInfo.engineVersion = VK_MAKE_VERSION(1, 0, 0);
-    appInfo.apiVersion = VK_API_VERSION_1_2;
+    appInfo.apiVersion = VK_API_VERSION_1_3;
 
     // get extensions from SDL
     std::vector<const char*> extensions = getRequiredExtensions(this->window);
